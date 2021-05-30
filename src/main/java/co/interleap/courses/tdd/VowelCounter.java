cc76cf8ecb7f5de724cdfc12e3e646c8e7bfcee4
Tajwinder <55351458+Tajwinder@users.noreply.github.com>
2021-05-30 15:29:37 +0530

    add first three tests for vowel counter

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,4 +1,7 @@
 package co.interleap.courses.tdd;
 
 public class VowelCounter {
+    public int countVowel(String str){
+        return str.length();
+    };
 }
--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -1,9 +1,28 @@
 package co.interleap.courses.tdd;
-
 import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 public class VowelCounterTest {
 
     @Test
-    public void dummy(){}
+    public void coutsToZeroWithEmptyString(){
+        VowelCounter vowelCounter= new VowelCounter();
+        int result= vowelCounter.countVowel("");
+        assertEquals(0,result);
+    }
+
+    @Test
+    public void shouldReturnOneWhenTheStrignContainsAvowelOnly(){
+        VowelCounter vowelCounter= new VowelCounter();
+        int result= vowelCounter.countVowel("e");
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void shouldReturnStringLengthWhenContainsVowelsOnly(){
+        VowelCounter vowelCounter= new VowelCounter();
+        int result= vowelCounter.countVowel("eao");
+        assertEquals(3, result);
+    }
 }
